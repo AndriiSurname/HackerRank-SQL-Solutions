0950f39ec887585be6bc5d26e4e9410e55d22ede
Andrii <101402199+AndriiSurname@users.noreply.github.com>
2024-05-09 19:39:11 -0400

    Create Ollivander's Inventory.sql

---
+++ Medium Solutions/Ollivander's Inventory.sql
@@ -0,0 +1,15 @@
+# Author: AndriiSurname
+SELECT W.id, WP.age, W.coins_needed, W.power
+FROM Wands W
+JOIN Wands_Property WP ON W.code = WP.code
+WHERE 
+     WP.is_evil = 0
+AND
+W.coins_needed = (
+                  SELECT MIN(coins_needed)
+                  FROM Wands W1
+                  JOIN Wands_Property WP1 ON W1.code = WP1.code
+                  WHERE W1.power = W.power and WP1.age = WP.age
+                 )
+ORDER BY W.power DESC,
+         WP.age DESC;
